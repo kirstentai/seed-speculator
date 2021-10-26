414d7f39627d862586b5d184b259d076dad612db
Kirsten <kirstent@Kirstens-MacBook-Pro.local>
2021-10-26 14:16:19 +1100

    providing startup name and amount raised

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -1,6 +1,6 @@
 # find pattern in url numbers
 
-# find start and end bounds
+# find start and end bounds: 5285 to 5942
 
 #loop through each url from start to end -- get request
 
@@ -14,13 +14,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="https://angelmatch.io/pitch_decks/5286"
+startup_num = 5285
+url="https://angelmatch.io/pitch_decks/" + str(startup_num)
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# extract startup name
+for tag in soup.find_all('div', class_="card-header-title name"):
+    print("Name: ",tag.text.strip())
+
 #pull out fund raised
-for tag in soup.find_all('div', class_="content"):
+for tag in soup.find_all('div', class_="custom-filter first-left"):
+    # custom-filter first-left -> box -> content -> field -> <p>
     p_finder = tag.find('p')
+
     if p_finder != None:
         print(p_finder.text.strip())
+
