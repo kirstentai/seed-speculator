edd26e1106b3864b5fd61dcc45642817796fea89
Kirsten Tai <kirsten.belle11@gmail.com>
2021-11-02 16:06:55 +1100

    test_startup

---
+++ 01_collection/tests/test_startup.py
@@ -0,0 +1,12 @@
+import scraper
+import requests
+from bs4 import BeautifulSoup
+from scraper.startup import Startup
+
+def test_name_retrieves_correct_result():
+    url = "https://angelmatch.io/pitch_decks/5808"
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    name = Startup.get_name(soup)
+    assert name == "Slidebean"
