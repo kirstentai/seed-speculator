eb27982f7bbc1277d5c5af023a3f6426c0325a61
Kirsten Tai <kirsten.belle11@gmail.com>
2021-11-15 14:30:55 +1100

    refactored startup functions and working main()

--- 01_collection/scraper/routines.py
+++ 01_collection/scraper/routines.py
@@ -1,58 +1,35 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
-from scraper.startup import Startup
-from tqdm import tqdm
+from startup import get_name, get_amount, get_pdflink
+from tqdm import *
+import time    
 
-from scraper.io import write_results
-
+# start and end bounds: 5285 to 5942 (657 items)
 def main():
-    init_num = 5801
-    start_num = 5801
-    end_num = 5942
+
+
+    start_num = 5285
+    end_num = 5290
     nopdf_link = 0
 
-    results = []
-
     while start_num <= end_num:
-        # start and end bounds: 5285 to 5942 (657 items)
-        url="https://angelmatch.io/pitch_decks/" + str(start_num)
-        page = requests.get(url)
-
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        startup_obj = Startup(start_num)
-        startup_name = Startup.get_name(startup_obj, soup)
-        startup_amt = Startup.get_amount(startup_obj, soup)
-        startup_link = Startup.get_pdflink(startup_obj, soup)
+    # for i in range(5285, 5943):
+        startup_name = get_name(start_num)
+        startup_amt = get_amount(start_num)
+        startup_link = get_pdflink(start_num)
 
         if startup_link == None:
             nopdf_link += 1
+            print("No link: {}".format(nopdf_link))
 
-        # output = "Name: {}\nAmount: {}\nLink: {}".format(startup_name, startup_amt, startup_link)
-        # print(output)
-
-        results.append({"start_num": start_num, "startup_name":startup_name, "startup_amt": startup_amt, "startup_link": startup_link})
-
-        # output_to_write = "{},{},{},{}\n".format(start_num, startup_name, startup_amt, startup_link)
-        # # print(output_to_write)
-
-        # #Testing items properly stored
-        # # print("test objs: name-{},amt-{},link-{}".format(startup_obj.name, startup_obj.amount, startup_obj.pdf_link))
-
-        # with open('output.txt', 'a') as parsed_file:
-        # #     # mode set to a. appends to existing
-        #     parsed_file.write(output_to_write)
-        
-        # start_num += 1
-    
-    write_results(results)
-
-    with open('tracker.txt', 'a') as track_file:
-        output = "\n{} to {}: {} items with no link.".format(init_num, end_num, nopdf_link)
+        output = "Num: {}\nName: {}\nAmount: {}\nLink: {}".format(start_num, startup_name, startup_amt, startup_link)
         print(output)
-        track_file.write(output)
+        
+        start_num += 1
 
-    track_file.close()
-    parsed_file.close()
 
-    #extract pdf content with pymupdf
+# with tqdm(total=100, file=sys.stdout) as pbar:
+#     for i in range(10):
+#         main()
+#         pbar.update(10)
--- 01_collection/scraper/startup.py
+++ 01_collection/scraper/startup.py
@@ -2,49 +2,56 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-class Startup:
-
-    # def __init__(self, name, link_num, amount, pdf_link):
-    def __init__(self, link_num):
-
-        self.link_num = link_num
-
-    def get_name(self, soup_link):
-
-        for tag in soup_link.find_all('div', class_="card-header-title name"):
-            name = tag.text.strip()
-            self.name = name
-            return name
     
+def get_name(start_num):
+    url="https://angelmatch.io/pitch_decks/" + str(start_num)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-    def get_amount(self, soup_link):
-
-        for tag in soup_link.find_all('div', class_="custom-filter first-left"):
-            # custom-filter first-left -> box -> content -> field -> <p>
-            p_finder = tag.find('p')
-
-            if p_finder != None:
-                amount_raised_string = p_finder.text.strip()
-                amount = str(re.split(" ", amount_raised_string)[3])
-                self.amount = amount
-
-                return amount
-
-            return "No amount found."
-    
-
-    def get_pdflink(self, soup_link):
-
-        for tag in soup_link.find_all('button'):
-            # dl_link = tag.find('a', download_="")
-            # all_buttons =  soup.find('button')
-            airtable_link = tag.find(href=True)
-            href_link = airtable_link['href']
+    for tag in soup.find_all('div', class_="card-header-title name"):
+        name = tag.text.strip()
+        return name
 
 
-            # note: implement if href is defined, download pdf else skip and raise warning in script
-            if re.search(r"airtable", href_link):
-                self.pdf_link = href_link
-                return href_link
-            # else:
-            #     return "No PDF link found."
+def get_amount(start_num):
+    url="https://angelmatch.io/pitch_decks/" + str(start_num)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for tag in soup.find_all('div', class_="custom-filter first-left"):
+        # custom-filter first-left -> box -> content -> field -> <p>
+        p_finder = tag.find('p')
+
+        if p_finder != None:
+            amount_raised_string = p_finder.text.strip()
+            amount = str(re.split(" ", amount_raised_string)[3])
+
+            return amount
+
+        return "No amount found."
+
+
+def get_pdflink(start_num):
+    url="https://angelmatch.io/pitch_decks/" + str(start_num)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for tag in soup.find_all('button'):
+        # dl_link = tag.find('a', download_="")
+        # all_buttons =  soup.find('button')
+        airtable_link = tag.find(href=True)
+        href_link = airtable_link['href']
+
+        file_name = str(start_num) + ".pdf"
+
+
+        # note: implement if href is defined, download pdf else skip and raise warning in script
+        if re.search(r"airtable", href_link):
+            response = requests.get(href_link)
+
+            with open(file_name, 'wb') as f:
+                f.write(response.content)
+            return href_link
+        # else:
+        #     return "No PDF link found."
+
