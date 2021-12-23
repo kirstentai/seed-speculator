51625fc863db1a6f18fb9eed5ef05bdc6cc3b86c
Kirsten Tai <kirsten.belle11@gmail.com>
2021-12-23 20:05:04 +1100

    section 1 scraper output attributes of startups

--- 01_collection/scraper/io.py
+++ 01_collection/scraper/io.py
@@ -1,2 +1,13 @@
-def write_results(array_of_dicts):
-    ...
+""" Write dictionary to json"""
+import os
+import json
+
+
+def write_results(filename, dictionary):
+    f = open(filename, "w+")
+    json.dump(dictionary, f, indent=2)
+    # for d in list:
+    # json.dumps(list)
+    
+    f.close()
+    print("List successfully converted to JSON.")
--- 01_collection/scraper/startup.py
+++ 01_collection/scraper/startup.py
@@ -43,9 +43,6 @@ def get_pdflink(start_num):
         href_link = airtable_link['href']
 
         file_name = str(start_num) + ".pdf"
-
-
-        # note: implement if href is defined, download pdf else skip and raise warning in script
         # try:
         if re.search(r"airtable", href_link):
         # print("regex: {}".format(href_link))
@@ -54,6 +51,40 @@ def get_pdflink(start_num):
             with open(file_name, 'wb') as f:
                 f.write(response.content)
             return href_link
-        # else:
-        #     return "No PDF link found."
 
+
+def get_round(start_num):
+    url="https://angelmatch.io/pitch_decks/" + str(start_num)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    for tag in soup.find_all('div', class_="content"):
+        link = tag.find(href=True)
+        href_link = link['href']
+        if re.search(r"by_stage", href_link):
+            p_finder = tag.find('a')
+            amount_raised_string = p_finder.text.strip()
+            # print(amount_raised_string)
+
+            return amount_raised_string
+
+
+def get_industries(start_num):
+    url="https://angelmatch.io/pitch_decks/" + str(start_num)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    industry_list = []
+    for tag in soup.find_all('div', class_="content"):
+        
+        for href_elements in tag.find_all('a', href=True):
+            href_link = href_elements['href']
+            
+            if re.search(r"by_category", href_link):
+                industry_list.append(href_elements.text.strip())
+    
+    unique_set = set(industry_list)
+    final_industry_list = list(unique_set)
+    # print(f"list: {final_industry_list}")
+
+    return final_industry_list
