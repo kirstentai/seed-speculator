8552e2a51e3884951dc5b870fbcdc755f3d89bb5
Kirsten Tai <kirsten.belle11@gmail.com>
2021-12-24 00:50:53 +1100

    01, 02 sections minor modifications

--- 02_extraction/extract/output.py
+++ 02_extraction/extract/output.py
@@ -3,7 +3,7 @@ import os
 import json
 
 def write_results(filename, dictionary):
-    f = open(filename, "w")
-    json.dump(dictionary, f)
+    f = open(filename, "w+")
+    json.dump(dictionary, f, indent=2)
     f.close()
     print("Dictionary successfully converted to JSON.")
