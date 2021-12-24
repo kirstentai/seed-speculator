eb7d8575247f9720673b456963be00d47fea2bb4
Kirsten Tai <kirsten.belle11@gmail.com>
2021-12-25 00:00:15 +1100

    nlp: get text from each startup

--- 03_analysis/analyse/nlp.py
+++ 03_analysis/analyse/nlp.py
@@ -1,12 +1,13 @@
 """ Sentiment analysis on pdf text"""
 import json
-
+import os.path
 
 
 def main():
     # read in json files
     file_name = 'output_final.json'
-    f = open(file_name)
+    file_path = get_filepath(file_name)
+    f = open(file_path)
     data = json.load(f)
 
     # extracting text per startup, list of strings
@@ -14,5 +15,15 @@ def main():
         if startup['num'] == '5286':
             print(startup['text']) # list type
 
+
+def get_filepath(filename):
+    current_directory = os.path.dirname(__file__)
+    parent_directory = os.path.split(current_directory)[0]
+    filepath = os.path.join(parent_directory, filename)
+
+    if os.path.exists(filepath): 
+        return filepath
+    else:
+        return None
 
 main()  
--- 03_analysis/merge/io.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-def write_results(filename, dataset):
-    f = open(filename, "w+")
-    json.dump(dataset, f, indent=2)
-    f.close()
-    print("Exported to JSON.")
